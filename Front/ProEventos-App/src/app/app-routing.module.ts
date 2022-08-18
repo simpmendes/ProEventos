5128e8ad1e5e2425395fdbc6cb80d10d76eec22f
simpmendes <simpmendes@gmail.com>
2022-08-18 15:00:05 -0300

    Layout Responsivo + Forms

--- Front/ProEventos-App/src/app/app-routing.module.ts
+++ Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,17 +1,37 @@
+import { RegistrationComponent } from './components/user/registration/registration.component';
+import { LoginComponent } from './components/user/login/login.component';
+import { EventoListaComponent } from './components/eventos/evento-lista/evento-lista.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { ContatosComponent } from './components/contatos/contatos.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { EventoDetalheComponent } from './components/eventos/evento-detalhe/evento-detalhe.component';
 import { EventosComponent } from './components/eventos/eventos.component';
 import { PalestrantesComponent } from './components/palestrantes/palestrantes.component';
-import { PerfilComponent } from './components/perfil/perfil.component';
+import { PerfilComponent } from './components/user/perfil/perfil.component';
+import { UserComponent } from './components/user/user.component';
 
 const routes: Routes = [
-  { path: 'eventos', component: EventosComponent},
+  {
+    path: 'user', component: UserComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'registration', component: RegistrationComponent}
+    ]
+  },
+  {path: 'eventos', redirectTo: 'eventos/lista'},
+  { path: 'user/perfil', component: PerfilComponent},
+  {
+    path: 'eventos', component: EventosComponent,
+    children: [
+      {path: 'detalhe/:id', component: EventoDetalheComponent},
+      {path: 'detalhe', component: EventoDetalheComponent},
+      {path: 'lista', component: EventoListaComponent},
+    ]
+  },
   { path: 'palestrantes', component: PalestrantesComponent},
-  { path: 'dashboard', component: DashboardComponent},
-  { path: 'perfil', component: PerfilComponent},
   { path: 'contatos', component: ContatosComponent},
+  { path: 'dashboard', component: DashboardComponent},
   { path: '', redirectTo: 'dashboard', pathMatch: 'full'},
   { path: '**', redirectTo: 'dashboard', pathMatch: 'full'}
 ];
--- Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/components/eventos/eventos.component.ts
@@ -12,72 +12,8 @@ import { NgxSpinnerService } from 'ngx-spinner';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: Evento[] = [];
-  public eventosFiltrados: Evento[] = [];
-  public widthImg: number = 50;
-  public marginImg: number = 2;
-  public showImg: boolean = true;
-  private _filtroLista: string = '';
-  modalRef?: BsModalRef;
-  message?: string;
+  ngOnInit(): void {
 
-  public get filtroLista(): string{
-    return this._filtroLista;
-  }
-
-  public set filtroLista(value: string){
-    this._filtroLista = value;
-    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
-  }
-  public filtrarEventos(filtrarPor: string): Evento[]{
-    filtrarPor = filtrarPor.toLocaleLowerCase();
-    return this.eventos.filter(
-      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1 ||
-      evento.local.toLocaleLowerCase().indexOf(filtrarPor) !== -1
-      );
-
-  }
-
-  constructor(private eventoService: EventoService,
-              private modalService: BsModalService,
-              private toastr: ToastrService,
-              private spinner: NgxSpinnerService) { }
-
-  public ngOnInit(): void {
-    this.spinner.show();
-    this.getEventos();
-  }
-
-  public alterarImg(): void{
-    this.showImg = !this.showImg;
-  }
-
-  public getEventos(): void{
-    this.eventoService.getEventos().subscribe({
-      next: (response: Evento[]) => {
-       this.eventos = response,
-       this.eventosFiltrados = response;
-     },
-      error: (error: any) => {
-        this.spinner.hide();
-        this.toastr.error('Erro ao carregar os Eventos', 'Erro');
-      },
-      complete: () => this.spinner.hide()
-    });
-  }
-
-  openModal(template: TemplateRef<any>): void {
-    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
-  }
-
-  confirm(): void {
-    this.modalRef?.hide();
-    this.toastr.success('O Evento foi deletado com Sucesso.', 'Deletado!');
-  }
-
-  decline(): void {
-    this.message = 'Declined!';
-    this.modalRef?.hide();
   }
 
 
